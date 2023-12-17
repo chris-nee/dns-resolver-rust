7365fefda482d063fb9b4bebb61a96dd58275c2b
chris-nee <lingngeehung@gmail.com>
2023-12-17 15:07:42 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
 // Uncomment this block to pass the first stage
 use std::net::UdpSocket;
+use std::str;
+
+// DNS Header
+pub struct DnsHeader {
+    bytes: [u8; 12],
+}
+
+impl DnsHeader {
+    pub fn new(
+        id: u16,
+        qr: u8,
+        opcode: u8,
+        aa: u8,
+        tc: u8,
+        rd: u8,
+        ra: u8,
+        z: u8,
+        rcode: u8,
+        qdcount: u16,
+        ancount: u16,
+        nscount: u16,
+        arcount: u16,
+    ) -> Self {
+        let bytes = [
+            (id >> 8) as u8,
+            (id << 8) as u8,
+            qr << 7 | opcode << 3 | aa << 2 | tc << 1 | rd,
+            ra << 7 | z << 4 | rcode,
+            qdcount as u8,
+            (qdcount >> 8) as u8,
+            ancount as u8,
+            (ancount >> 8) as u8,
+            nscount as u8,
+            (nscount >> 8) as u8,
+            arcount as u8,
+            (arcount >> 8) as u8,
+        ];
+
+        Self { bytes }
+    }
+
+    pub fn encode(&self) -> &[u8] {
+        &self.bytes //.as_bytes()
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,7 +59,21 @@ fn main() {
             Ok((size, source)) => {
                 let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                println!("Received data: {:?}", &buf[..size]);
+
+                // Simply for printing
+                match str::from_utf8(&buf[0..size]) {
+                    Ok(received_str) => {
+                        println!("parsed: {}", received_str)
+                    }
+                    Err(_e) => {
+                        println!("Err parsing utf8")
+                    }
+                };
+
+                let header = DnsHeader::new(1234, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+                let response = header.bytes; // .encode();
+
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");

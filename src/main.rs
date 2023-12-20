cf5732537fcfd0bd6dc8ff5f29cd77d6f8d594cf
chris-nee <lingngeehung@gmail.com>
2023-12-20 09:43:11 +0800

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ fn main() {
                 response.extend(question.encode().into_iter());
 
                 udp_socket
-                    .send_to(response.as_bytes(), source)
+                    .send_to(response, source)
                     .expect("Failed to send response");
             }
             Err(e) => {

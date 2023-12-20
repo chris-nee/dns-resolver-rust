a5f338209405a8e78ce671639e9062d42fc9cd9e
chris-nee <lingngeehung@gmail.com>
2023-12-20 09:54:42 +0800

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,103 @@
-// Uncomment this block to pass the first stage
-// use nom::AsBytes;
 use std::net::UdpSocket;
-
-// DNS Header
-pub struct DnsHeader {
-    bytes: [u8; 12],
+// The whole must be 12 bytes when encoded.
+// which is 3 x 4 bytes.
+#[derive(Default)]
+struct Header {
+    packet_id: u16,                 // 16 bit
+    query_response_indicator: bool, // 1  bit
+    operation_code: u8,             // 4  bits
+    authoritative_answer: bool,     // 1  bit
+    truncation: bool,               // 1  bit
+    recursion_desired: bool,        // 1  bit
+    recursion_available: bool,      // 1  bit
+    reserved: u8,                   // 3  bits
+    response_code: u8,              // 4  bits
+    question_count: u16,            // 16 bits
+    answer_record: u16,             // 16 bits
+    authority_record_count: u16,    // 16 bits
+    additional_record_count: u16,   // 16 bits
 }
-
-impl DnsHeader {
-    pub fn new(
-        id: u16,
-        qr: u8,
-        opcode: u8,
-        aa: u8,
-        tc: u8,
-        rd: u8,
-        ra: u8,
-        z: u8,
-        rcode: u8,
-        qdcount: u16,
-        ancount: u16,
-        nscount: u16,
-        arcount: u16,
-    ) -> Self {
-        let bytes = [
-            (id >> 8) as u8,
-            id as u8,
-            qr << 7 | opcode << 3 | aa << 2 | tc << 1 | rd,
-            ra << 7 | z << 4 | rcode,
-            qdcount as u8,
-            (qdcount >> 8) as u8,
-            ancount as u8,
-            (ancount >> 8) as u8,
-            nscount as u8,
-            (nscount >> 8) as u8,
-            arcount as u8,
-            (arcount >> 8) as u8,
-        ];
-
-        Self { bytes }
-    }
-
-    pub fn encode(&self) -> &[u8] {
-        &self.bytes //.as_bytes()
-    }
-}
-
-// DNS Question
-pub struct DnsQuestion {
-    domain_name: String,
-    query_type: u16,
-    query_class: u16,
-}
-
-impl DnsQuestion {
-    pub fn new(domain_name: String, query_type: u16, query_class: u16) -> Self {
+impl Header {
+    fn new(id: u16) -> Self {
         Self {
-            domain_name,
-            query_type,
-            query_class,
+            packet_id: id,
+            query_response_indicator: true,
+            ..Self::default()
         }
     }
-
-    fn encode_domain_name(&self) -> Vec<u8> {
-        let mut bytes = Vec::new();
-        self.domain_name.split('.').for_each(|x| {
-            bytes.extend((x.len() as u8).to_be_bytes());
-            bytes.extend(x.as_bytes());
-        });
-
-        bytes.push(0u8);
-        bytes
+    fn encode(&self) -> [u8; 12] {
+        let part_two: u8 = (self.query_response_indicator as u8) << 7
+            | (self.operation_code << 3)
+            | (self.authoritative_answer as u8) << 2
+            | (self.truncation as u8) << 1
+            | (self.recursion_desired as u8);
+        let part_three: u8 =
+            (self.recursion_available as u8) << 7 | (self.reserved as u8) << 4 | self.response_code;
+        [
+            (self.packet_id >> 8) as u8,
+            self.packet_id as u8,
+            part_two,
+            part_three,
+            (self.question_count >> 8) as u8,
+            self.question_count as u8,
+            (self.answer_record >> 8) as u8,
+            self.answer_record as u8,
+            (self.authority_record_count >> 8) as u8,
+            self.authority_record_count as u8,
+            (self.additional_record_count >> 8) as u8,
+            self.additional_record_count as u8,
+        ]
     }
+}
+#[derive(Default)]
+struct Question {
+    name: Vec<u8>,
+    type_: u16,
+    class: u16,
+}
+impl Question {
+    fn new(name: &str) -> Self {
+        let mut encoded_name = vec![];
+        for label in name.split(".") {
+            encoded_name.push(label.len() as u8);
+            encoded_name.append(&mut label.as_bytes().to_owned());
+        }
+        encoded_name.push(0u8);
+        Self {
+            name: encoded_name,
+            ..Self::default()
+        }
+    }
+    fn with_type(self, type_: u16) -> Self {
+        Self { type_, ..self }
+    }
+    fn with_class(self, class: u16) -> Self {
+        Self { class, ..self }
+    }
+    fn encode(&self) -> Vec<u8> {
+        let mut question_encoded = vec![];
+        question_encoded.append(&mut self.name.to_owned());
+        question_encoded.push((self.type_ >> 8) as u8);
+        question_encoded.push(self.type_ as u8);
+        question_encoded.push((self.class >> 8) as u8);
+        question_encoded.push(self.class as u8);
 
-    pub fn encode(&self) -> Vec<u8> {
-        let mut bytes = Vec::new();
-        bytes.extend(self.encode_domain_name());
-        // bytes.extend(self.query_type.to_be_bytes());
-        bytes.push((self.query_type >> 8) as u8);
-        bytes.push(self.query_type as u8);
-        bytes.push((self.query_class >> 8) as u8);
-        bytes.push(self.query_class as u8);
-        // bytes.extend(self.query_class.to_be_bytes());
-
-        bytes
+        question_encoded
     }
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
-
+    let mut header = Header::new(1234);
+    header.question_count = 1;
+    let question = Question::new("codecrafters.io").with_type(1).with_class(1);
+    let mut response = vec![];
+    response.extend(question.encode());
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
-                println!("Received data: {:?}", &buf[..size]);
-
-                let mut response = Vec::new();
-
-                let header = DnsHeader::new(1234, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
-                response.extend(header.encode().into_iter());
-
-                let question = DnsQuestion::new("codecrafters.io".to_string(), 1, 1);
-                response.extend(question.encode().into_iter());
-
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");

ae9add1aec74c5c55b14ad2675971e4dcc987b0c
chris-nee <lingngeehung@gmail.com>
2023-12-22 12:37:49 +0800

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,21 @@ impl DNSQuestion {
             query_class,
         }
     }
-    fn encoded_domain_name(&self) -> Vec<u8> {
-        let mut bytes = Vec::new();
-        for part in self.domain_name.split(".") {
-            bytes.extend((part.len() as u8).to_be_bytes());
-            bytes.extend(part.as_bytes())
+
+    fn to_be_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
+
+        let parts = self.domain_name.split(".");
+        for part in parts {
+            bytes.push(part.len() as u8);
+            bytes.extend_from_slice(part.as_bytes());
         }
         bytes.push(0);
-        bytes
-    }
-    fn to_be_bytes(&self) -> Vec<u8> {
-        let mut bytes = Vec::new();
-        bytes.extend(self.encoded_domain_name());
+
         bytes.extend(self.query_type.to_be_bytes());
         bytes.extend(self.query_class.to_be_bytes());
-        bytes
+
+        bytes.try_into().expect("err")
     }
 }
 struct DNSHeader {
@@ -81,16 +81,17 @@ impl DNSHeader {
     // To big-endian bytes
     fn to_be_bytes(&self) -> [u8; 12] {
         let mut bytes: Vec<u8> = Vec::new();
+
         bytes.extend(&self.id.to_be_bytes());
-        bytes.push((self.qr << 7) | (self.opcode << 3) | (self.aa << 2) | (self.tc << 1) | self.rd);
-        bytes.push((self.ra << 7) | (self.z << 4) | (self.r_code));
+        bytes.push(&self.qr << 7 | &self.opcode << 3 | &self.aa << 2 | &self.tc << 1 | &self.rd);
+        bytes.push(&self.ra << 7 | &self.z << 4 | &self.r_code);
+
         bytes.extend(&self.qd_count.to_be_bytes());
         bytes.extend(&self.an_count.to_be_bytes());
         bytes.extend(&self.rs_count.to_be_bytes());
         bytes.extend(&self.ar_count.to_be_bytes());
-        bytes
-            .try_into()
-            .expect("DNSHeader did not match expected length")
+
+        bytes.try_into().expect("[u8; 12]")
     }
 }
 fn main() {
@@ -104,14 +105,16 @@ fn main() {
             Ok((size, source)) => {
                 let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
-                let header = DNSHeader::new(1234, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+                let header = DNSHeader::new(1234, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0);
 
                 let question = DNSQuestion::new("codecrafters.io".to_string(), 1, 1);
 
                 let mut response = Vec::new();
+
                 response.extend(header.to_be_bytes());
 
                 response.extend(question.to_be_bytes());
+
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");

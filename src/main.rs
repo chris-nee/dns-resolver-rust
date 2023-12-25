01ba7c2ac8738d83b7a7ac10d892dca12d9ebf6b
chris-nee <lingngeehung@gmail.com>
2023-12-25 21:12:07 +0800

    s

--- src/main.rs
+++ src/main.rs
@@ -307,7 +307,6 @@ fn main() {
                 let mut q_offset = HEADER_SIZE;
                 for _ in 0..header.qd_count {
                     let question = DNSQuestion::from_bytes(&byte_arr, q_offset);
-
                     q_offset += question.to_bytes().len();
 
                     // Forward to dns server
@@ -340,10 +339,10 @@ fn main() {
                     for _ in 0..new_header.qd_count {
                         let new_qn = DNSQuestion::from_bytes(&recv_buf_vec, inner_offset);
                         inner_offset += new_qn.to_bytes().len();
-                        question_packets.push(new_qn.clone());
+                        question_packets.push(new_qn);
 
                         let new_ans = DNSAnswer::from_bytes(&recv_buf_vec, inner_offset);
-                        answer_packets.push(new_ans.clone());
+                        answer_packets.push(new_ans);
                     }
                 }
 

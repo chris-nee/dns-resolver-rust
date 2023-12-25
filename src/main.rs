ee285c4db84fe80d7a11191940ec6b351f60cdd8
chris-nee <lingngeehung@gmail.com>
2023-12-25 09:00:08 +0800

    stage final

--- src/main.rs
+++ src/main.rs
@@ -298,9 +298,8 @@ fn main() {
 
                 for _ in 0..header.qd_count {
                     let question = DNSQuestion::from_bytes(&byte_arr, q_offset);
+                    q_offset += question.clone().to_bytes().len();
                     question_packets.push(question.clone());
-
-                    q_offset += question.to_bytes().len();
 
                     // Forward to dns server
                     let mut query = Vec::new();
@@ -334,7 +333,7 @@ fn main() {
                     for _ in 0..new_header.qd_count {
                         let new_qn = DNSQuestion::from_bytes(&recv_buf_vec, inner_offset);
                         inner_offset += new_qn.to_bytes().len();
-                        println!("The qn {:}", new_qn.domain_name);
+                        println!("The qn {:}", new_qn.domain_t name);
 
                         let new_ans = DNSAnswer::from_bytes(&recv_buf_vec, inner_offset);
                         answer_packets.push(new_ans.clone());

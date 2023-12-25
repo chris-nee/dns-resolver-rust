6a4ecbf5fba0da6f35bad26675b7d5a1b9e6681e
chris-nee <lingngeehung@gmail.com>
2023-12-25 18:18:00 +0800

    stage final

--- src/main.rs
+++ src/main.rs
@@ -173,7 +173,7 @@ impl DNSQuestion {
                 let label_len: usize = byte_arr[idx_offset] as usize;
                 str_item.extend_from_slice(&byte_arr[idx_offset + 1..idx_offset + 1 + label_len]);
                 str_item.push(46); // "."
-                idx = idx_offset + 1;
+                idx = idx_offset + label_len + 1;
                 println!("LEN: ({})", label_len);
                 println!(
                     "STR: ({:})",
